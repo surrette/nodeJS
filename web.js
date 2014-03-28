462140bc6c74a8c9d739dbba5038f627bf9c2be6
surrette <steve.surrette@gmail.com>
2014-03-28 18:14:24 +0000

    Added connection to mongolab

--- web.js
+++ web.js
@@ -1,8 +1,18 @@
-var express = require('express');
+var express = require('express')
+  , mongoose = require('mongoose');
 var app = express();
 app.use(express.logger());
 
+mongoose.connect("mongodb://steve:steve@ds033797.mongolab.com:33797/heroku_app23345901");
+var Cat = mongoose.model('Cat', { name: String });
+
+
 app.get('/', function(request, response) {
+	var kitty = new Cat({ name: 'Zildjian' });
+	kitty.save(function (err) {
+	  if (err) // ...
+ 	 console.log('meow');
+    });
   response.send('Hello World! database test change');
 });
 
